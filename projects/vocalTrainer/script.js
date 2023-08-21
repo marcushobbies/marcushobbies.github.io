9ce99941c9b0b481356180c3cbfc43a32583ea3b
Marcus Porfilio <marcus@porfilio.dev>
2023-08-21 00:08:24 -0400

    Added note display to Vocal Trainer

--- projects/vocalTrainer/script.js
+++ projects/vocalTrainer/script.js
@@ -24,6 +24,7 @@ with the above license.
 */
 
 var pitchElement = document.getElementById("pitch");
+var noteElement = document.getElementById("note");
 
 var source;
 var audioContext;
@@ -175,9 +176,11 @@ function findNote(){
 function updateDisplay(value){
     if(value === -1){
         pitchElement.innerHTML = "Too Quiet";
+        noteElement.innerHTML = "N/A";
         return;
     }
     pitchElement.innerHTML = value.toPrecision(5) + "Hz";
+    noteElement.innerHTML = findRelativeNote(value);
     heldNote = value;
 
 
@@ -202,6 +205,131 @@ function createStaff(gapSize, yOffset){
         staff.moveTo(0, (y-bassYOffset)*gapSize + bassYOffset);
         staff.lineTo(canvasWidth, (y-bassYOffset)*gapSize + bassYOffset);
     }
+}
+
+var halfStepsFromA4 = new Map();
+halfStepsFromA4.set(-57, 'C0');
+halfStepsFromA4.set(-56, 'C#0');
+halfStepsFromA4.set(-55, 'D0');
+halfStepsFromA4.set(-54, 'D#0');
+halfStepsFromA4.set(-53, 'E0');
+halfStepsFromA4.set(-52, 'F0');
+halfStepsFromA4.set(-51, 'F#0');
+halfStepsFromA4.set(-50, 'G0');
+halfStepsFromA4.set(-49, 'G#0');
+halfStepsFromA4.set(-48, 'A0');
+halfStepsFromA4.set(-47, 'A#0');
+halfStepsFromA4.set(-46, 'B0');
+
+halfStepsFromA4.set(-45, 'C1');
+halfStepsFromA4.set(-44, 'C#1');
+halfStepsFromA4.set(-43, 'D1');
+halfStepsFromA4.set(-42, 'D#1');
+halfStepsFromA4.set(-41, 'E1');
+halfStepsFromA4.set(-40, 'F1');
+halfStepsFromA4.set(-39, 'F#1');
+halfStepsFromA4.set(-38, 'G1');
+halfStepsFromA4.set(-37, 'G#1');
+halfStepsFromA4.set(-36, 'A1');
+halfStepsFromA4.set(-35, 'A#1');
+halfStepsFromA4.set(-34, 'B1');
+
+halfStepsFromA4.set(-33, 'C2');
+halfStepsFromA4.set(-32, 'C#2');
+halfStepsFromA4.set(-31, 'D2');
+halfStepsFromA4.set(-30, 'D#2');
+halfStepsFromA4.set(-29, 'E2');
+halfStepsFromA4.set(-28, 'F2');
+halfStepsFromA4.set(-27, 'F#2');
+halfStepsFromA4.set(-26, 'G2');
+halfStepsFromA4.set(-25, 'G#2');
+halfStepsFromA4.set(-24, 'A2');
+halfStepsFromA4.set(-23, 'A#2');
+halfStepsFromA4.set(-22, 'B2');
+
+halfStepsFromA4.set(-21, 'C3');
+halfStepsFromA4.set(-20, 'C#3');
+halfStepsFromA4.set(-19, 'D3');
+halfStepsFromA4.set(-18, 'D#3');
+halfStepsFromA4.set(-17, 'E3');
+halfStepsFromA4.set(-16, 'F3');
+halfStepsFromA4.set(-15, 'F#3');
+halfStepsFromA4.set(-14, 'G3');
+halfStepsFromA4.set(-13, 'G#3');
+halfStepsFromA4.set(-12, 'A3');
+halfStepsFromA4.set(-11, 'A#3');
+halfStepsFromA4.set(-10, 'B3');
+
+halfStepsFromA4.set(-9, 'C4');
+halfStepsFromA4.set(-8, 'C#4');
+halfStepsFromA4.set(-7, 'D4');
+halfStepsFromA4.set(-6, 'D#4');
+halfStepsFromA4.set(-5, 'E4');
+halfStepsFromA4.set(-4, 'F4');
+halfStepsFromA4.set(-3, 'F#4');
+halfStepsFromA4.set(-2, 'G4');
+halfStepsFromA4.set(-1, 'G#4');
+halfStepsFromA4.set(0, 'A4');
+halfStepsFromA4.set(1, 'A#4');
+halfStepsFromA4.set(2, 'B4');
+
+halfStepsFromA4.set(3, 'C5');
+halfStepsFromA4.set(4, 'C#5');
+halfStepsFromA4.set(5, 'D5');
+halfStepsFromA4.set(6, 'D#5');
+halfStepsFromA4.set(7, 'E5');
+halfStepsFromA4.set(8, 'F5');
+halfStepsFromA4.set(9, 'F#5');
+halfStepsFromA4.set(10, 'G5');
+halfStepsFromA4.set(11, 'G#5');
+halfStepsFromA4.set(12, 'A5');
+halfStepsFromA4.set(13, 'A#5');
+halfStepsFromA4.set(14, 'B5');
+
+halfStepsFromA4.set(15, 'C6');
+halfStepsFromA4.set(16, 'C#6');
+halfStepsFromA4.set(17, 'D6');
+halfStepsFromA4.set(18, 'D#6');
+halfStepsFromA4.set(19, 'E6');
+halfStepsFromA4.set(20, 'F6');
+halfStepsFromA4.set(21, 'F#6');
+halfStepsFromA4.set(22, 'G6');
+halfStepsFromA4.set(23, 'G#6');
+halfStepsFromA4.set(24, 'A6');
+halfStepsFromA4.set(25, 'A#6');
+halfStepsFromA4.set(26, 'B6');
+
+halfStepsFromA4.set(27, 'C7');
+halfStepsFromA4.set(28, 'C#7');
+halfStepsFromA4.set(29, 'D7');
+halfStepsFromA4.set(30, 'D#7');
+halfStepsFromA4.set(31, 'E7');
+halfStepsFromA4.set(32, 'F7');
+halfStepsFromA4.set(33, 'F#7');
+halfStepsFromA4.set(34, 'G7');
+halfStepsFromA4.set(35, 'G#7');
+halfStepsFromA4.set(36, 'A7');
+halfStepsFromA4.set(37, 'A#7');
+halfStepsFromA4.set(38, 'B7');
+
+function findRelativeNote(freq){
+    let halfSteps = Math.log(freq/440)/Math.log(Math.pow(2,1/12));
+    let note = "A4";
+
+    if(halfSteps > 0){
+        note = halfStepsFromA4.get(Math.ceil(halfSteps));
+        if(halfSteps % 1 < 0.5){
+            note = halfStepsFromA4.get(Math.floor(halfSteps));
+        }
+    }
+    if(halfSteps < 0){
+        note = halfStepsFromA4.get(Math.ceil(halfSteps));
+        if(halfSteps % 1 < -0.5){
+            note = halfStepsFromA4.get(Math.floor(halfSteps));
+        }
+    }
+
+    return note;
 }
 
 var currentNotePosition = 200;
